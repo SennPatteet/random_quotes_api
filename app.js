3e7622ee1a479b15cb4cd251d59a246b8bef1929
Senn Pateet <senn.patteet@hotmail.com>
2019-02-20 14:03:20 +0100

    new package.json

--- app.js
+++ app.js
@@ -43,21 +43,20 @@ app.use((req, res, next) => {
 //put '/quotes' in path by default
 app.use('/quotes', quoteRoutes);
 
+//error handling
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
 
-// //error handling
-// app.use((req, res, next) => {
-//   const error = new Error('Not found');
-//   error.status = 404;
-//   next(error);
-// });
-//
-// app.use((error, req, res, next) => {
-//   res.status(error.status || 500);
-//   res.json({
-//     error: {
-//       message: error.message
-//     }
-//   })
-// });
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  })
+});
 
 module.exports = app;
